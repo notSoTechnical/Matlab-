2d3df6f2b9be6398542cba51b62d2717ba3fdd74
notSoTechnical <50722184+notSoTechnical@users.noreply.github.com>
2020-02-01 16:45:54 -0500

    Add files via upload

---
+++ runnngSumOfaSignal.m
@@ -0,0 +1,9 @@
+plot(input_signal);
+output_signal = cumsum(input_signal);
+subplot(2, 1, 1);
+plot(input_signal, 'r');
+title('Input Signal');
+subplot(2,1,2);
+plot (output_signal, 'g');
+title('Output Signal from Running Sum');
+shg
